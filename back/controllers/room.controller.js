d3a3e6d17c022dfb64d97fcc4a6c33cbbd59eea3
JeremyB257 <bertelootjerem@gmail.com>
2022-10-10 16:47:59 +0200

    add getRoom & getAllRooms controller

--- back/controllers/room.controller.js
+++ back/controllers/room.controller.js
@@ -1,16 +1,51 @@
-import Room from '../models/room.model.js';
+import RoomModel from '../models/room.model.js';
+import HotelModel from '../models/hotel.model.js';
 
 //Read - Get
-export const getRoom = async (req, res, next) => {};
+export const getRoom = async (req, res, next) => {
+  try {
+    const room = await RoomModel.findById(req.params.id);
+    res.status(200).json(room);
+  } catch (err) {
+    next(err);
+  }
+};
 
-export const getAllRooms = async (req, res, next) => {};
+export const getAllRooms = async (req, res, next) => {
+  try {
+    const rooms = await RoomModel.find();
+    res.status(200).json(rooms);
+  } catch (err) {
+    next(err);
+  }
+};
 
 // Create - post
-export const createRoom = async (req, res, next) => {};
+export const createRoom = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
 
 // Update - put
-export const updateRoom = async (req, res, next) => {};
-export const updateRoomAvailability = async (req, res, next) => {};
+export const updateRoom = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
+export const updateRoomAvailability = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
 
 // Delete
-export const deleteRoom = async (req, res, next) => {};
+export const deleteRoom = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};

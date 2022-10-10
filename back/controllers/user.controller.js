7d756c5b03dfb21d6fd771d469e09cea99b65321
JeremyB257 <bertelootjerem@gmail.com>
2022-10-10 16:38:01 +0200

    add updateUser & deleteUser controller

--- back/controllers/user.controller.js
+++ back/controllers/user.controller.js
@@ -20,41 +20,19 @@ export const getUser = async (req, res, next) => {
 };
 
 export const updateUser = async (req, res, next) => {
-  const userObject = { ...req.body };
-  delete userObject._userId;
-  UserModel.findOne({ _id: req.params.id })
-    .then((user) => {
-      if (user._id != req.auth.userId) {
-        res.status(401).json({ message: 'Not authorized' });
-      } else {
-        UserModel.updateOne({ _id: req.params.id }, { ...userObject, _id: req.params.id })
-          .then(() => res.status(200).json({ message: 'Profil modifié!' }))
-          .catch((error) => res.status(401).json({ error }));
-      }
-    })
-    .catch((error) => {
-      res.status(400).json({ error });
-    });
+  try {
+    const updatedUser = await UserModel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    next(err);
+  }
 };
 
 export const deleteUser = async (req, res, next) => {
-  UserModel.findOne({ _id: req.params.id })
-    .then((user) => {
-      if (user._id != req.auth.userId) {
-        res.status(401).json({ message: 'Not authorized' });
-      } else {
-        const filename = user.picture.split('/profil/')[1];
-        fs.unlink(`../front/public/uploads/profil/${filename}`, () => {
-          UserModel.deleteOne({ _id: req.params.id })
-            .then(() => {
-              res.status(200).json({ message: 'Profil supprimé !' });
-            })
-            .catch((error) => res.status(401).json({ error }));
-        });
-      }
-    })
-    .catch((error) => {
-      res.status(500).json({ error });
-      console.log(error);
-    });
+  try {
+    await UserModel.findByIdAndDelete(req.params.id);
+    res.status(200).json('User has been deleted.');
+  } catch (err) {
+    next(err);
+  }
 };

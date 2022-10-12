495749281a78fbdfa254c6ffd7bf6788f0c36141
JeremyB257 <bertelootjerem@gmail.com>
2022-10-12 09:51:32 +0200

    add error reducer

--- front/src/reducers/index.jsx
+++ front/src/reducers/index.jsx
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import usersReducer from './users.reducer';
 import userReducer from './user.reducer';
+import errorReducer from './error.reducer';
 
 export default combineReducers({
   userReducer,
   usersReducer,
+  errorReducer,
 });

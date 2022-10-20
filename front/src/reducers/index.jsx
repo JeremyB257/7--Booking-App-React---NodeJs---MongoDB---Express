20d9a1425fd488885b943a25417c97c9717bcbb2
JeremyB257 <bertelootjerem@gmail.com>
2022-10-20 16:31:53 +0200

    add hotel reducer

--- front/src/reducers/index.jsx
+++ front/src/reducers/index.jsx
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 
 import userReducer from './user.reducer';
 import usersReducer from './users.reducer';
+import hotelReducer from './hotel.reducer';
 import hotelsReducer from './hotels.reducer';
 import errorReducer from './error.reducer';
 
 export default combineReducers({
   userReducer,
   usersReducer,
+  hotelReducer,
   hotelsReducer,
   errorReducer,
 });

285cc3cade5e3f324a68320a24cccbdc9cd0f2a5
JeremyB257 <bertelootjerem@gmail.com>
2022-10-25 12:25:11 +0200

    map data => hotel => thumbnail

--- front/src/components/Home/Lodging.jsx
+++ front/src/components/Home/Lodging.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const Lodging = () => {
+const Lodging = (props) => {
+  console.log(props);
   return (
     <section id="lodging" className="lodging">
-      <div class="establishment">
-        <h1>Hébergement à </h1>
+      <div className="establishment">
+        <h1>Hébergement à {props.destination}</h1>
+        <div className="establishment__card">
+          {props.data.map((hotel) => (
+            <div className="thumbnail">
+              <NavLink to={`/hotel/${hotel._id}`}>
+                <img className="thumbnail__img" src={hotel.photos[0]} alt={hotel.title}></img>
+                <h2 className="thumbnail__title">{hotel.title}</h2>
+                <p className="thumbnail__description">Nuit à partir de {hotel.cheapestPrice}€</p>
+                <div className="thumbnail__rating">
+                  <i className={hotel.rating > 0 ? 'fa-solid fa-star blue_star' : ' fa-solid fa-star grey_star'}></i>
+                  <i className={hotel.rating > 1 ? 'fa-solid fa-star blue_star' : ' fa-solid fa-star grey_star'}></i>
+                  <i className={hotel.rating > 2 ? 'fa-solid fa-star blue_star' : ' fa-solid fa-star grey_star'}></i>
+                  <i className={hotel.rating > 3 ? 'fa-solid fa-star blue_star' : ' fa-solid fa-star grey_star'}></i>
+                  <i className={hotel.rating > 4 ? 'fa-solid fa-star blue_star' : ' fa-solid fa-star grey_star'}></i>
+                </div>
+              </NavLink>
+            </div>
+          ))}
+        </div>
       </div>
-      <div class="featured-establishment"></div>
+      <div className="featured-establishment"></div>
     </section>
   );
 };
